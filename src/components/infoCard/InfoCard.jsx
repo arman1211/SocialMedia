4d537c07bbbffa7a8d57023633067d7ff11c23f8
arman1211 <[sharfuddinarman512@gmail.com]>
2022-09-19 13:54:34 +0600

    added modal for update profile

--- src/components/infoCard/InfoCard.jsx
+++ src/components/infoCard/InfoCard.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './InfoCard.css'
 import { UilPen } from '@iconscout/react-unicons'
+import ProfileModal from '../profileModal/ProfileModal'
 
 const InfoCard = () => {
+    const [modalOpened, setModalOpened] = useState(false)
     return (
         <div className="infoCard">
             <div className="infoHeader">
                 <h4>Your info</h4>
-                <UilPen />
+                <UilPen onClick={() => setModalOpened(true)} />
+                <ProfileModal modalOpened={modalOpened} setModalOpened={setModalOpened} />
 
             </div>
             <div className="info">
